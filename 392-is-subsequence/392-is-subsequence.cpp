330838a207a297705b47d4fd410aed7ab7d2dadd
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-03-03 01:28:57 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (72.95%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         return checkSubsequence(s,t, 0, 0);
     }
     
-    bool checkSubsequence(string s, string t, int ind1, int ind2){
+    bool checkSubsequence(string &s, string &t, int ind1, int ind2){
         if(ind1 == s.size())return true;
         
         if(s[ind1] == t[ind2]) return checkSubsequence(s,t,ind1+1, ind2+1);
