04a553957a4772c97e1e4e62bf205e0760ee47b8
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-06-08 19:05:30 +0600

    Time: 0 ms (100.00%), Space: 6.2 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -3,9 +3,9 @@ public:
     int removePalindromeSub(string s) {
      int a=0, b=0, ispalindrome=1, len =s.size();
 
-      for(int i=0; i<len; i++){
-        if(s[i]=='a')a=1;
-        if(s[i]=='b')b=1;
+      for(int i=0; i<=len/2; i++){
+        if(s[i]=='a' || s[len-i-1] == 'a')a=1;
+        if(s[i]=='b'|| s[len-i-1] == 'b')b=1;
         if(s[i]!=s[len-i-1])ispalindrome = 10000;
               }
       
