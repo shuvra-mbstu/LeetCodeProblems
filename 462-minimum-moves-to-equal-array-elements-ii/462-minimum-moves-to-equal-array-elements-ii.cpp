b96728c8e60ceb588f07c60cb48c3f3d2cf7b656
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-06-30 15:22:12 +0600

    Time: 31 ms (5.90%), Space: 10.8 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,11 +3,11 @@ public:
     int minMoves2(vector<int>& nums) {
         int len = nums.size();
       sort(nums.begin(), nums.end());
-      int avg = nums[len/2];
+      int median = nums[len/2];
       
       int ans =0;
       for(int i=0; i<len; i++){
-        ans += abs(nums[i] - avg);
+        ans += abs(nums[i] - median);
       }
       return ans;
     }
