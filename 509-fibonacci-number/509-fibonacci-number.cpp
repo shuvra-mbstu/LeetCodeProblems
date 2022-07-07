22479c0116b840761e88ba32dfb12d05cf9641c3
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-07-07 15:01:40 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (80.07%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+        int fib_num[40];
+
     int fib(int n) {
         if(n==0)return 0;
         if(n==1)return 1;
         
+        if(fib_num[n]!=0)return fib_num[n];
         
-        return fib(n-1)+fib(n-2);
+        return fib_num[n] = fib(n-1)+fib(n-2);
     }
 };
