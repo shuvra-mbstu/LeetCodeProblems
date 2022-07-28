3f47d26b0cb0a674095e329889385033ba65deef
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-07-28 11:13:14 +0600

    Time: 17 ms (44.89%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+      if(s.size()!=t.size())return false;
+
       sort(s.begin(), s.end());
       sort(t.begin(), t.end());
-      
-      if(s.size()!=t.size())return false;
-      
+            
       int i=0, j=0;
       for(; i<t.size() && j<s.size(); i++, j++){
         if(s[j]!=t[i])return false;
