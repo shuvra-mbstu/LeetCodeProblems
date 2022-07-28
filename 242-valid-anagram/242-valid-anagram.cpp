994c73c6b2a7e12a3fe76d80083d395161ffbf23
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-07-28 11:12:49 +0600

    Time: 23 ms (22.29%), Space: 7.3 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -4,13 +4,13 @@ public:
       sort(s.begin(), s.end());
       sort(t.begin(), t.end());
       
+      if(s.size()!=t.size())return false;
+      
       int i=0, j=0;
       for(; i<t.size() && j<s.size(); i++, j++){
         if(s[j]!=t[i])return false;
       }
       
-      if(i==t.size() && j ==s.size())
       return true;
-      else return false;
     }
 };
