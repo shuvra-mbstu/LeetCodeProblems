7fb2acd598fa9e069aa814b9023898115086fefe
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-08-08 15:48:20 +0600

    Time: 2366 ms (5.00%), Space: 38.1 MB (17.16%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-   
-    int function(int i,vector<int>&nums,int prev,vector<vector<int>>&dp){
-        if(i>=nums.size())return 0;
-        
-        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
-        
-        int take=0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            take=1+function(i+1,nums,i,dp);
-        }
-        
-        int nt=0;
-        nt=function(i+1,nums,prev,dp);
-        
-        return dp[i][prev+1]=max(take,nt);
-        
-    }
+  int dp[2505][2505];
     
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        
-        return function(0,nums,-1,dp);
+      
+      if(nums.size() == 1) return 1;
+      memset(dp, -1, sizeof(dp));
+             
+        return res(0, -1, nums);
     }
+  
+  int res(int curr, int last, vector<int>& nums){
+    if(curr>= nums.size())return 0;
+    
+    if(dp[curr][last+1]!= -1) return dp[curr][last+1];
+    
+    int res1=0, res2=0;
+    if(last== -1 || nums[curr] > nums[last]){
+      res1 = res(curr+1, curr, nums) + 1;
+    }
+    
+    res2 = res(curr+1, last, nums);
+    return dp[curr][last+1] = max(res1, res2);
+  }
 };
