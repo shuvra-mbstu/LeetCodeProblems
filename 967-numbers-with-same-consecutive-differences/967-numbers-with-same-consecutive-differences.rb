844cc9029ff4f1dadbed14fe98618129a1476a7f
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-09-04 03:56:31 +0600

    Time: 107 ms (100.00%), Space: 211 MB (100.00%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.rb
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.rb
@@ -1,38 +1,32 @@
-class Solution {
+# @param {Integer} n
+# @param {Integer} k
+# @return {Integer[]}
     
-    vector<int> ans;
-    
-    void Solve(int len , int &k , int num, int digit)
-    {
-        // Base Case
-        if(len == 0 )
-        {
-                ans.push_back(num);
-           return ;
-        }
-        
-        // Next Possible digits will have range from 0 - 9
-        for(int i = 0 ; i<10 ; i++)
-        {
-            // Checking for given condition 
-            if(abs(i-digit) == k)
-                Solve(len-1 , k, num*10 + i, i);
-        }
-    }
-    
-public:
-    vector<int> numsSameConsecDiff(int n, int k) {
-        
-        
-        // First Digit will have range from  1 - 9 
-        // We have passed digit as 4th variable to have the last inserted digit
-        for(int i = 1 ; i<=9 ; i++)
-        {
-             Solve(n-1 , k, i, i);
-        }
-        
-        return ans;
-        
-		// Pls Upvote If You Like It !!
-    }
-};
+def consecutive_difference_numbers(num, len, k)
+  last_digit = num%10
+  numbers =[]
+  
+  if len == 0
+    return [num]
+  end
+
+  for i in 0..9
+    if (last_digit - i).abs == k
+      # puts (last_digit - i).abs
+      # puts num
+      numbers += consecutive_difference_numbers(num*10+i, len-1, k)
+    end
+  end
+  
+  numbers
+end
+
+def nums_same_consec_diff(n, k)
+  numbers = []
+  
+  for i in 1..9
+    numbers += consecutive_difference_numbers(i, n-1, k)
+  end
+  
+  return numbers
+end
