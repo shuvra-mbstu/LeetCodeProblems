42585c13086efc8782db65dae20e6928c90d4bf8
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-09-22 15:27:46 +0600

    Time: 138 ms (71.59%), Space: 212.6 MB (51.14%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.rb
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.rb
@@ -1,16 +1,5 @@
-class Solution {
-public:
-    string reverseWords(string s) {
-     int len = s.size(), last=0;
-      
-      for(int i=0; i<=len; i++){
-        if(s[i]== ' ' || i == len){
-          reverse(s.begin()+last,s.begin()+i);
-          // cout<<s<<" ";//<<s.begin()+last+1<<" "<<s.begin()+i<<endl;
-          last = i + 1;
-        }
-      }
-
-      return s;
-    }
-};
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+    s.split.map(&:reverse).join(" ")
+end
