28e57835768f2edd8218c74389e3e884338b07f6
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-10-10 14:43:32 +0600

    Time: 5 ms (20.18%), Space: 6.1 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -6,9 +6,6 @@ public:
       if(len == 1) return "";
       
       int first_half = len/2;
-      int second_half = len/2;
-
-      if(len%2 == 0) second_half--;
       
       for(int i=0; i<first_half; i++){
         if(palindrome[i] != 'a'){
