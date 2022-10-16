ca06a36366ac83de6b37f5234115a5d1155cd643
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-10-16 23:57:31 +0600

    Time: 102 ms (20.61%), Space: 20.8 MB (35.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+      vector<vector<string>> anagrams;
+        map<string, vector<string>> anagram_map;
+      
+      int len = strs.size();
+      
+      for(int i=0; i<len; i++){
+        string ss = strs[i];
+        sort(ss.begin(), ss.end());
+        
+        anagram_map[ss].push_back(strs[i]);
+      }
+      
+      for(auto i : anagram_map){
+        anagrams.push_back(i.second);
+        // cout<<i.first<<endl;
+      }
+      return anagrams;
+    }
+};
