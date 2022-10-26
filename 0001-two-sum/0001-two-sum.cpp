e498d5d87ccb89dde7f2a3e671c8b66374b170ab
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-10-26 14:00:39 +0600

    Time: 17 ms (84.43%), Space: 11.4 MB (15.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int,int> mpp;
         int len = nums.size();
-        
-        for(int i=0; i<len; i++){
-            if(mpp[target-nums[i]]){
-                return {  mpp[target-nums[i]]-1, i};
-            }
-            mpp[nums[i]]=i+1;
-        }
-        return {};
+      
+      unordered_map<int, int> mpp;
+      
+      for(int i=0 ;i<len; i++){
+        if(mpp[target-nums[i]] != 0) return {mpp[target-nums[i]]-1, i}; 
+        mpp[nums[i]] = i+1;
+      }
+      return {};
     }
 };
