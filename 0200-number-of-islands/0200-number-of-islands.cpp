cc091df3470e7f169e90762275efafc256584f4c
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-10-27 15:27:26 +0600

    Time: 64 ms (52.09%), Space: 12.4 MB (55.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,33 @@
-
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        
-int count =0;
-
-for(int I=0; I<grid.size(); I++){
-for(int j=0; j<grid[0].size(); j++){
-       if(grid[I][j] == '1'){
-count ++;
-visit(I, j, grid);
+        int row = grid.size();
+        int col = grid[0].size();
+        int count=0;
+      
+      for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+          if(grid[i][j]=='1'){
+            count++;
+            DFS(i,j, grid);
+          }    
+        }
+      }
+      return count;
     }
-  }
-}
-return count;
-    }
-
-void visit( int ind1, int ind2, vector<vector<char>>& grid){
-  grid[ind1][ind2] = '0';
   
-int x[] = { 0, 0, -1, 1};
-int y[] = { -1, 1, 0, 0};
-	for(int i=0; i<4; i++){
-		
-		int x_coordinator = ind1 + x[i];
-		int y_coordinator = ind2 + y[i];
-
-		if(x_coordinator < grid.size() && x_coordinator >=0 && y_coordinator < grid[0].size() && y_coordinator>=0 && grid[x_coordinator][y_coordinator] == '1'){
-		visit(x_coordinator, y_coordinator, grid);
+  void DFS(int i, int j, vector<vector<char>> & grid){
+    grid[i][j]='0';
+    int x[] = {0, 0, 1, -1};
+    int y[] = {-1, 1, 0, 0};
+    
+    for(int a=0; a<4; a++){
+      int xx = i + x[a];
+      int yy = j + y[a];
+      
+      if(grid.size() > xx && xx>=0 && yy >=0 && yy < grid[0].size() && grid[xx][yy] == '1'){
+        DFS(xx, yy, grid);
+      }
     }
   }
-}
-
 };
