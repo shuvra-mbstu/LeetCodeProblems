25af8d9f0d16b4ee223ada8825d67509bc0d5633
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2022-10-27 20:24:02 +0600

    Time: 70 ms (45.78%), Space: 12.4 MB (44.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,13 +1,18 @@
 class Solution {
-public:
+public:  
+            bool visit[307][307];
+
     int numIslands(vector<vector<char>>& grid) {
+      
         int row = grid.size();
         int col = grid[0].size();
         int count=0;
       
+      
+
       for(int i=0; i<row; i++){
         for(int j=0; j<col; j++){
-          if(grid[i][j]=='1'){
+          if(grid[i][j]=='1' && !visit[i][j]){
             count++;
             DFS(i,j, grid);
           }    
@@ -16,8 +21,8 @@ public:
       return count;
     }
   
-  void DFS(int i, int j, vector<vector<char>> & grid){
-    grid[i][j]='0';
+  void DFS(int i, int j, vector<vector<char>>& grid){
+    visit[i][j]=1;
     int x[] = {0, 0, 1, -1};
     int y[] = {-1, 1, 0, 0};
     
@@ -25,7 +30,7 @@ public:
       int xx = i + x[a];
       int yy = j + y[a];
       
-      if(grid.size() > xx && xx>=0 && yy >=0 && yy < grid[0].size() && grid[xx][yy] == '1'){
+      if(grid.size() > xx && xx>=0 && yy >=0 && yy < grid[0].size() && !visit[xx][yy] && grid[xx][yy]=='1'){
         DFS(xx, yy, grid);
       }
     }
