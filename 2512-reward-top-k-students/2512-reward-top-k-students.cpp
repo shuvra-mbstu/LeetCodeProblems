17dc917f450cb0a9d5d176ffc1af12ef6b310866
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2024-01-14 13:23:35 +0100

    Time: 226 ms (68.02%), Space: 75.7 MB (31.53%) - LeetHub

--- 2512-reward-top-k-students/2512-reward-top-k-students.cpp
+++ 2512-reward-top-k-students/2512-reward-top-k-students.cpp
@@ -1,41 +1,45 @@
-bool cmp(const pair<int,int> &a, const pair<int, int> &b){
+bool cmp (const pair<int, int> &a, pair<int, int> &b){
     if(a.first==b.first) return a.second<b.second;
-
+    
     return a.first>b.first;
 }
 
 class Solution {
 public:
-
     vector<int> topStudents(vector<string>& positive_feedback, vector<string>& negative_feedback, vector<string>& report, vector<int>& student_id, int k) {
-        
         unordered_map<string, int> feedbacks;
         
-        for(auto p : positive_feedback){ feedbacks[p]= 3; }
-        
-        for(auto p : negative_feedback){ feedbacks[p]= -1; }
-        
-        vector<pair<int, int>> students;
+        for(auto pf : positive_feedback){ feedbacks[pf]= 3; }
+        for(auto nf : negative_feedback){ feedbacks[nf]= -1; }
         
         int len = report.size();
+        vector<pair<int, int>> students_info;
         
+        // cout<<len<<endl;
         for(int i=0; i<len; i++){
             
-            stringstream ss(report[i]);
+            // cout<<report[i]<<endl;
+           stringstream ss(report[i]);
             
+            int count=0;
             string s;
-            int score =0;
             
-            while(ss>>s){ score += feedbacks[s]; }
-
-            students.push_back({score, student_id[i]});
+            while(ss>>s){ 
+                // cout<<s<<endl;
+                count += feedbacks[s]; }
+            
+            // cout<<count<<" "<<student_id[i]<<endl;
+            students_info.push_back({count, student_id[i]});
         }
-        sort(students.begin(), students.end(), cmp);
-        vector<int> res;
+        
+        sort(students_info.begin(), students_info.end(), cmp);
+        
+        vector<int> result;
         
         for(int i=0; i<k; i++){
-            res.push_back(students[i].second);
+           result.push_back(students_info[i].second); 
         }
-        return res;
+        
+        return result;
     }
 };
