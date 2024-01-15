3fc8a68b952eedff635808c568e38ec7f8c9747a
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2024-01-15 22:43:18 +0100

    Time: 0 ms (100.00%), Space: 6.6 MB (26.08%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    
+    bool helper(string& str, int i, int n, vector<long long>& ans)
+    {
+        // base case, if we have reached the end of str and ans.size() >= 3, than return true
+        
+        if(i == n && ans.size() >= 3)
+            return true;
+        
+        // must take unsigned long long because long long is giving overflow
+        
+        unsigned long long num = 0;
+        
+        for(int j = i; j < n; j++)
+        {
+            // calculate num
+            
+            num = num * (long long) 10 + (long long) (str[j] - '0');
+            
+            // this can't be include in ans
+            
+            if(num > LLONG_MAX)
+                return false;
+            
+            // handling the leading zero
+            
+            if(str[i] == '0' && j > i)
+                return false;
+            
+            // if ans array has less than 2 numbers then simply insert into ans array
+            
+            // if ans array has greater than equal 2 numbers then check if last two numbers sum up to num then insert into ans array
+            
+            if(ans.size() < 2 || ans.back() + ans[ans.size() - 2] == num)
+            {
+                ans.push_back(num);
+                
+                if(helper(str, j + 1, n, ans))
+                    return true;
+                
+                // if added num doesn't lead to ans then backtrack
+                
+                ans.pop_back();
+            }
+            
+        }
+        
+        return false;
+    }    
+    
+    bool isAdditiveNumber(string str) {
+        
+        int n = str.size();
+        
+        vector<long long> ans;
+        
+        return helper(str, 0, n, ans);
+    }
+};
