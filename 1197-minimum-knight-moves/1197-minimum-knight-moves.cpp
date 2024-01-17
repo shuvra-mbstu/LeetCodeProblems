32b0143608dbcc6610d7e5114288eaba31cc8409
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2024-01-17 01:30:10 +0100

    Time: 25 ms (82.96%), Space: 14.1 MB (80.71%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,51 +1,21 @@
 class Solution {
+    
+    unordered_map<string, int> DP;
+    
+    int dfs(int x, int y){
+        string s = to_string(x) + "#" + to_string(y);
+        
+        // cout<<s<<endl;
+        if(DP.find(s) != DP.end()) return DP[s];
+        
+        if(x+y == 0) return 0;
+        if(x+y == 2) return 2;
+        
+        return DP[s] = min(dfs(abs(x-2), abs(y-1)), dfs(abs(x-1), abs(y-2))) +1;
+    }
 public:
-    int minKnightMoves(int x, int y) {
-     
-        vector<pair<int,int>> moves ={{2,-1}, {2, 1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}};
-        
-        int xx = abs(x);
-        int yy = abs(y);
-        
-        queue<pair<int, int>> que;
-        
-        set<pair<int, int>> vis;
-        
-        int dis = 0;
-        
-        que.push({0,0});
-        vis.insert({0,0});
-        
-        while(!que.empty()){
-            
-            int siz = que.size();
 
-            for(int i=0; i<siz; i++){
-
-                auto node = que.front();
-                que.pop();
-
-                int indx = node.first;
-                int indy = node.second;
-            
-                if(indx == xx && indy == yy) return dis;
-                
-                for(auto &j : moves){
-                    int nx = indx + j.first;
-                    int ny = indy + j.second;
-                    
-                    if(nx < -2 || ny < -2) continue;
-                    if(nx > xx+2 || ny > yy+2) continue;
-                    if(vis.find({nx, ny}) != vis.end()) continue;
-                    
-                    vis.insert({nx, ny});
-                    que.push({nx, ny});
-                }
-                
-            }
-            dis++;
-            
-        }
-        return 0;
+    int minKnightMoves(int x, int y) {        
+        return dfs(abs(x), abs(y));
     }
 };
