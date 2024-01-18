075aceccb3359ea2c404021bdf6cfe23da9adba6
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2024-01-19 00:26:14 +0100

    Time: 9 ms (76.59%), Space: 10.7 MB (78.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-   
-    int function(int i,vector<int>&nums,int prev,vector<vector<int>>&dp){
-        if(i>=nums.size())return 0;
-        
-        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
-        
-        int take=0;
-        if(prev==-1 || nums[i]>nums[prev]){
-            take=1+function(i+1,nums,i,dp);
-        }
-        
-        int nt=0;
-        nt=function(i+1,nums,prev,dp);
-        
-        return dp[i][prev+1]=max(take,nt);
-        
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        vector<int> LIS;
         
-        return function(0,nums,-1,dp);
+        for(auto &num: nums){
+            auto place = lower_bound(LIS.begin(), LIS.end(), num) - LIS.begin();
+            
+            if(place == LIS.size()){
+                LIS.push_back(num);
+            } 
+            else LIS[place] = num;
+        }
+        return LIS.size();
     }
 };
