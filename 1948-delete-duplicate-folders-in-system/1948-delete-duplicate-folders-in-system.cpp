05146a175bc7f8536e150a928fdb3e3b604762dd
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2024-01-18 02:13:44 +0100

    Time: 595 ms (93.94%), Space: 262.4 MB (86.36%) - LeetHub

--- 1948-delete-duplicate-folders-in-system/1948-delete-duplicate-folders-in-system.cpp
+++ 1948-delete-duplicate-folders-in-system/1948-delete-duplicate-folders-in-system.cpp
@@ -1,62 +1,78 @@
-struct Node{
-    string name;
-    map<string, Node*> next;
-    bool del = false;
-    Node(string n = "") : name(n) {}
+/*
+Important to note:
+1) Here we use Map in Node as to unordered_map. This is due to the fact that when you will
+	run dfs or serialize on the subfolders althrough they have exact same folder structure. 
+	The key in the duplicates map might be different when you use unordered_map.
+2) It is very important to define the structure of root->child i.e. by adding ( and ) as necessary
+	as different folder structure might also lead to same keys in duplicates again causing
+	problems.
+*/
+struct Node {
+    map<string, Node*> child;
+    bool toDelete = false;
 };
 
 class Solution {
-    void addPath(Node *node, vector<string> &path){
-        for(auto &s : path){
-            if(node->next.count(s) == 0) node-> next[s] = new Node(s);
-            
-            node = node->next[s];
-        }
-    }
-    
-    unordered_map<string, Node*> seen;
-    
-    
-    string dedupe(Node *node){
-        string subfolder;
-        
-        for(auto &[name, next] : node->next){
-            subfolder += dedupe(next);
-        }
-        
-        if(subfolder.size()){
-            if(seen.count(subfolder)){
-                seen[subfolder]-> del = node->del = true;
-            }
-            else{
-                seen[subfolder] = node;
-            }
-        }
-        return "(" + node->name + subfolder + ")";
-    }
+    unordered_map<string, vector<Node*>> duplicates;
     vector<vector<string>> ans;
-    
-    vector<string> path;
-    
-    void getPath( Node *node){
-        if(node->del) return;
-        
-        path.push_back(node->name);
-        ans.push_back(path);
-        
-        for(auto &[name, next] : node-> next){
-            getPath(next);
-        }
-        path.pop_back();
-    }
 public:
-
-    vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
-        Node root;
+    string dfs(Node* root) {
+        string currValue = "";
+        if (root->child.size() > 0) {
+            currValue += "(";
+        } else {
+            return "";
+        }
+        for (auto& child : root->child) {
+            currValue += child.first + dfs(child.second);
+        }
+        currValue += ")";
         
-        for(auto &path : paths) addPath(&root, path);
-        dedupe(&root);
-        for(auto &[name, next] : root.next) getPath(next);
+        if (currValue != "") {
+            duplicates[currValue].push_back(root);
+        }
+        
+        return currValue;
+    }
+    
+    void insert(Node* root, vector<string>& path, int i) {
+        if (i >= path.size()) return;
+        if (root->child.find(path[i]) == root->child.end()) {
+            root->child[path[i]] = new Node();
+        }
+        insert(root->child[path[i]], path, i + 1);
+    }
+    
+    void removeUnwantedNodes(Node* root, vector<string>& temp) {
+        if (root->toDelete) return;
+        for(auto& child : root->child) {
+            temp.push_back(child.first);
+            removeUnwantedNodes(child.second, temp);
+            temp.pop_back();
+        }
+        if (temp.size() > 0) {
+            ans.push_back(temp);
+        }
+    }
+    
+    vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
+        Node* root = new Node();
+        for (auto& path : paths) {
+            insert(root, path, 0);
+        }
+        dfs(root);
+        
+        for (auto& dup : duplicates) {
+            if (dup.second.size() > 1) {
+                for (auto& node : dup.second) {
+                    node->toDelete = true;
+                }
+            }
+        }
+        
+        vector<string> temp;
+        removeUnwantedNodes(root, temp);
+        
         return ans;
     }
 };
