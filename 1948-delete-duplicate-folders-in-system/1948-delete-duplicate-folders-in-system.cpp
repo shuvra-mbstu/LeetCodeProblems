ec474ae33e12912ba3a46ae1732447d133201439
shuvra-mbstu <21081859+shuvra-mbstu@users.noreply.github.com>
2024-01-21 03:51:50 +0100

    Time: 623 ms (73.68%), Space: 312.6 MB (15.79%) - LeetHub

--- 1948-delete-duplicate-folders-in-system/1948-delete-duplicate-folders-in-system.cpp
+++ 1948-delete-duplicate-folders-in-system/1948-delete-duplicate-folders-in-system.cpp
@@ -1,78 +1,74 @@
-/*
-Important to note:
-1) Here we use Map in Node as to unordered_map. This is due to the fact that when you will
-	run dfs or serialize on the subfolders althrough they have exact same folder structure. 
-	The key in the duplicates map might be different when you use unordered_map.
-2) It is very important to define the structure of root->child i.e. by adding ( and ) as necessary
-	as different folder structure might also lead to same keys in duplicates again causing
-	problems.
-*/
 struct Node {
     map<string, Node*> child;
-    bool toDelete = false;
+    bool toBeDeleted = false;
 };
+
 
 class Solution {
     unordered_map<string, vector<Node*>> duplicates;
-    vector<vector<string>> ans;
-public:
-    string dfs(Node* root) {
+    vector<vector<string>> result;
+    
+void add_node(Node* &root, vector<string> &path, int i){
+    if(i>=path.size()) return;
+    
+    if(root->child.find(path[i]) == root->child.end()){
+        root->child[path[i]] = new Node();
+        // cout<<path[i]<<endl;
+    }
+    add_node(root->child[path[i]], path, i+1);
+  }
+    string dfs(Node* &root){
+        if(root->child.size() == 0) return "";
+
         string currValue = "";
-        if (root->child.size() > 0) {
-            currValue += "(";
-        } else {
-            return "";
-        }
-        for (auto& child : root->child) {
-            currValue += child.first + dfs(child.second);
-        }
-        currValue += ")";
         
-        if (currValue != "") {
+        for(auto childNode : root->child){
+            currValue += '(' + childNode.first + dfs(childNode.second);
+        }
+         currValue += ')';
+        
+        if(currValue != "") {
             duplicates[currValue].push_back(root);
         }
-        
+            
         return currValue;
     }
     
-    void insert(Node* root, vector<string>& path, int i) {
-        if (i >= path.size()) return;
-        if (root->child.find(path[i]) == root->child.end()) {
-            root->child[path[i]] = new Node();
-        }
-        insert(root->child[path[i]], path, i + 1);
-    }
     
-    void removeUnwantedNodes(Node* root, vector<string>& temp) {
-        if (root->toDelete) return;
-        for(auto& child : root->child) {
+    void removeUnwantedNode(Node* &root, vector<string> & temp){
+        if(root->toBeDeleted) return;
+        
+        for(auto child : root->child){
             temp.push_back(child.first);
-            removeUnwantedNodes(child.second, temp);
+            removeUnwantedNode(child.second, temp);
+            // cout<<child.first<<endl;
             temp.pop_back();
         }
-        if (temp.size() > 0) {
-            ans.push_back(temp);
-        }
+        if(temp.size()>0)result.push_back(temp);
     }
+
+public:
     
     vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
         Node* root = new Node();
-        for (auto& path : paths) {
-            insert(root, path, 0);
+        
+        for(auto path : paths){
+            add_node(root, path, 0);
         }
+        
         dfs(root);
         
-        for (auto& dup : duplicates) {
-            if (dup.second.size() > 1) {
-                for (auto& node : dup.second) {
-                    node->toDelete = true;
+        for(auto dup : duplicates){
+            if(dup.second.size()>1){
+                for(auto &node: dup.second){
+                    node->toBeDeleted = true;
                 }
             }
         }
-        
         vector<string> temp;
-        removeUnwantedNodes(root, temp);
+        removeUnwantedNode(root, temp);
         
-        return ans;
+        return result;
     }
 };
+    
