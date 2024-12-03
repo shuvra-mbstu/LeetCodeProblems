6c7f1226b32f929e346d1391cef2c22ee5690e52
shuvra-debnath <21081859+shuvra-mbstu@users.noreply.github.com>
2024-12-03 22:03:16 +0100

    Time: 2 ms (21.84%), Space: 41.7 MB (37.53%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,22 +3,15 @@ class Solution {
         
         if(strs.length < 2) return strs[0];
         
-        StringBuilder st = new StringBuilder();
+        StringBuilder st = new StringBuilder(strs[0]);
         int j=0, k=0;
             
-            while(j<strs[0].length() && k<strs[1].length() && strs[0].charAt(j)== strs[1].charAt(k)){
-                st.append(strs[0].charAt(j));
-                j++;
-                k++;
-            }
-                
-        for(int i=2; i< strs.length; i++){
+        for(int i=1; i< strs.length; i++){
             j=0;
             k=0;
-            String temp = new String(strs[i]);
             StringBuilder ss = new StringBuilder();
             
-            while(j<st.length() && k< strs[i].length() && st.charAt(j) == temp.charAt(k)){
+            while(j<st.length() && k< strs[i].length() && st.charAt(j) == strs[i].charAt(k)){
                 ss.append(st.charAt(j));
                 j++;
                 k++;
