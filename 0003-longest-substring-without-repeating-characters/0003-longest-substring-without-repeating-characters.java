05163049516b67591b51ae6064b9c5bf5865f523
shuvra-debnath <21081859+shuvra-mbstu@users.noreply.github.com>
2024-12-13 05:10:21 +0100

    Time: 5 ms (87.43%), Space: 44.7 MB (39.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -7,13 +7,10 @@ class Solution {
         for(int right=0; right<s.length(); right++){
             char c = s.charAt(right);
             
-            if(!present.containsKey(c) || present.get(c) < left){
-                present.put(c, right);
+            if(present.containsKey(c) ){
+                left = Math.max(left, present.get(c)+1);
             }
-            else{
-                left = present.get(c) + 1;
-                present.put(c, right);
-            }
+            present.put(c, right);
              res = Math.max(res, right-left+1);
         }
         return res;
