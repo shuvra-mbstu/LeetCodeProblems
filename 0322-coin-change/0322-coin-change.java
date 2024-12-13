ec41838ccf4648ddb796025915da6af839c7f773
shuvra-debnath <21081859+shuvra-mbstu@users.noreply.github.com>
2024-12-13 20:31:53 +0100

    Time: 10 ms (99.72%), Space: 44.6 MB (46.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,14 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
+        int[] dp = new int[amount+3];
         Arrays.fill(dp, Integer.MAX_VALUE-1);
-        dp[0] = 0;
         
+        dp[0]=0;
         for(int coin : coins){
-            for(int i = coin; i<=amount; i++){
+            for(int i= coin; i<=amount; i++){
                 dp[i] = Math.min(dp[i], 1+dp[i-coin]);
             }
         }
-        
-        return dp[amount]== Integer.MAX_VALUE-1 ? -1 : dp[amount];
-        
+        return (dp[amount]== Integer.MAX_VALUE-1) ? -1 : dp[amount];
     }
 }
